0a277c95f3839d48835402bf655b530c39ab329e
Breezen <brizziemanuele@gmail.com>
2025-11-24 22:38:34 +0100

    Feat: implement a control in Email Construct for empty string

--- backend/boardgame-connect/src/main/java/boardgameconnect/model/Email.java
+++ backend/boardgame-connect/src/main/java/boardgameconnect/model/Email.java
@@ -3,9 +3,12 @@ package boardgameconnect.model;
 public class Email {
 
 	private String email;
-	
+
 	public Email(String email) {
 		super();
+		if (email == "") {
+			throw new IllegalArgumentException("Invalid email format");
+		}
 		this.email = email;
 	}
 
