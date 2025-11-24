c5fb7b1f1b1eaa95d7d372cc4c93952f696eec80
Breezen <brizziemanuele@gmail.com>
2025-11-24 22:20:29 +0100

    Test: create a failing test for a empty string in email

--- backend/boardgame-connect/src/test/java/boardgameconnect/model/EmailTest.java
+++ backend/boardgame-connect/src/test/java/boardgameconnect/model/EmailTest.java
@@ -1,6 +1,7 @@
 package boardgameconnect.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,4 +12,12 @@ public class EmailTest {
         Email email = new Email("user@domain.com");
         assertEquals("user@domain.com", email.getEmail());
     }
+	
+	@Test
+	void testCreateEmailWhenEmailStringIsEmptyStringShouldSThrow() {
+        Email email = new Email("");
+        assertThrows(IllegalArgumentException.class, () -> new Email(""));
+    }
+	
+	
 }
