37a5fb0b3f83976b8acbd48e57107b03e8d72bfc
Breezen <brizziemanuele@gmail.com>
2025-11-25 02:21:37 +0100

    Test: change name test case

--- backend/boardgame-connect/src/test/java/boardgameconnect/model/BoardgameTest.java
+++ backend/boardgame-connect/src/test/java/boardgameconnect/model/BoardgameTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class BoardgameTest {
 	
 	@Test
-	void testCreateBoardgameWhenNameTaxcodeAddressAreValidShouldCreateassociation() {
+	void testCreateBoardgameWhenValuesAreValidShouldCreateassociation() {
 		Boardgame bg = new Boardgame ("Terra Mystica",2,5,60,30);
 		assertEquals("Terra Mystica", bg.getName());
 		assertEquals(2, bg.getMinPlayer());
