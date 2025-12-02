54da76cec82264b7f5699eefd3b48536fa346bc7
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-12-02 14:53:11 +0100

    Update app html and css

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth-service';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly auth = inject(AuthService);
+}
