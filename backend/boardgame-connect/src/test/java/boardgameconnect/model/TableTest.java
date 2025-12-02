70454123012c7da594775754de1f0c3bf8009c00
Emanuele Brizzi <brizziemanuele@gmail.com>
2025-12-02 02:28:31 +0100

    Feat/table model (#21)
    * Test: add a failing test for Table creation
    
    * Feat: implementing Table class

---
+++ backend/boardgame-connect/src/test/java/boardgameconnect/model/TableTest.java
@@ -0,0 +1,14 @@
+package boardgameconnect.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TableTest {
+
+	@Test
+	void testCreateEmailWhenEmailStringIsValidShouldCreateEmail() {
+		Table t = new Table(TableSize.SMALL);
+		assertEquals(TableSize.SMALL, t.getSize());
+	}
+}
