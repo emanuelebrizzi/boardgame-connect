c8e9f57b8af5145f527292bface8c20e0ccbeb96
Breezen <brizziemanuele@gmail.com>
2025-12-15 14:29:55 +0100

    test: change AuthenticationServiceTest login test
    changed testLoginWhenValuesAreInvalidTest and change name in
    testLoginWhenCredentialsAreInvalidTest

--- backend/boardgame-connect/src/test/java/boardgameconnect/service/AuthenticationServiceTest.java
+++ backend/boardgame-connect/src/test/java/boardgameconnect/service/AuthenticationServiceTest.java
@@ -1,31 +1,52 @@
 package boardgameconnect.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import boardgameconnect.dao.UserRepository;
 import boardgameconnect.dto.LoginRequest;
+import boardgameconnect.model.Email;
+import boardgameconnect.model.Player;
+import boardgameconnect.model.User;
 
 class AuthenticationServiceTest {
 
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
     private AuthenticationService authenticationService;
 
     @BeforeEach
     void setUp() {
-        authenticationService = new AuthenticationService();
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void login_fail_with_wrong_password() {
+    void testLoginWhenCredentialsAreInvalidTest() {
+
+        User user = new Player(
+                "u_123",
+                new Email("mario.rossi@example.com"),
+                "password",
+                "Mario Rossi"
+        );
+
+        when(userRepository.findByEmail_Email("mario.rossi@example.com"))
+                .thenReturn(user);
+
         LoginRequest request =
                 new LoginRequest("mario.rossi@example.com", "wrong");
 
-        RuntimeException exception = assertThrows(
-                RuntimeException.class,
-                () -> authenticationService.login(request)
-        );
 
-        assertEquals("Credenziali non valide", exception.getMessage());
+        assertThrows(RuntimeException.class,
+                () -> authenticationService.login(request));
     }
+
 }
