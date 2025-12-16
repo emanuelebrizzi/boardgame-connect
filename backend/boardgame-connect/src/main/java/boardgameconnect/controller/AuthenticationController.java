8b5d9ee037639478ce3e0291ec9734f8b25a90ec
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-12-16 12:49:21 +0100

    Implement logic when the request is valid

--- backend/boardgame-connect/src/main/java/boardgameconnect/controller/AuthenticationController.java
+++ backend/boardgame-connect/src/main/java/boardgameconnect/controller/AuthenticationController.java
@@ -1,5 +1,29 @@
 package boardgameconnect.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import boardgameconnect.dto.LoginRequest;
+import boardgameconnect.dto.LoginResponse;
+import boardgameconnect.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/api/v1/login")
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+	this.authenticationService = authenticationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+	LoginResponse response = authenticationService.login(request);
+	return ResponseEntity.ok(response);
+    }
 
 }
