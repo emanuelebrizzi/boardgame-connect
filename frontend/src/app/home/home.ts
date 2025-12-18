afbb53c48d3377d2d79652d9af6609789e948615
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-12-18 19:19:49 +0100

    Implement guest guard

---
+++ frontend/src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home',
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {}
