5b2dfff89b1f2da515fef8aaad7b61a0e7a69eaa
Breezen <brizziemanuele@gmail.com>
2026-01-16 15:48:52 +0100

    change GameTableRepository

--- backend/boardgame-connect/src/main/java/boardgameconnect/dao/GameTableRepository.java
+++ backend/boardgame-connect/src/main/java/boardgameconnect/dao/GameTableRepository.java
@@ -1,11 +1,8 @@
 package boardgameconnect.dao;
 
-import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import boardgameconnect.model.GameTable;
@@ -15,12 +12,4 @@ public interface GameTableRepository extends JpaRepository<GameTable, String> {
 
 	List<GameTable> findByAssociationId(String associationId);
 
-	@Query("""
-			    SELECT t FROM GameTable t
-			    WHERE t.association.id = :associationId
-			    AND t.capacity >= :minCapacity
-			""")
-	List<GameTable> findAvailableTables(@Param("associationId") String associationId,
-			@Param("minCapacity") int minCapacity, @Param("startTime") Instant startTime,
-			@Param("endTime") Instant endTime);
 }
